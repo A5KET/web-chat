465df532508d8e33724ddc36809816882422f7c8
A5KET <asketinin@gmail.com>
2023-12-16 17:19:44 +0200

    Remove eslint google's extension

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,17 @@
 module.exports = {
   'env': {
     'browser': true,
-    'commonjs': true,
-    'es2021': true,
     'node': true,
   },
-  'extends': 'google',
-  'overrides': [
-    {
-      'env': {
-        'node': true,
-      },
-      'files': [
-        '.eslintrc.{js,cjs}',
-      ],
-      'parserOptions': {
-        'sourceType': 'script',
-      },
-    },
-  ],
   'parserOptions': {
+    'sourceType': 'module',
     'ecmaVersion': 'latest',
   },
   'rules': {
     'require-jsdoc': 0,
-    'indent': ['error', 2, {SwitchCase: 1}],
-    'quotes': [2, 'single', {'avoidEscape': true}],
-    'semi': [2, 'never'],
+    'indent': ['error', 2],
+    'quotes': ['error', 'single', { 'avoidEscape': true }],
+    'semi': ['error', 'never'],
+    'object-curly-spacing': [2, 'always']
   },
 }
