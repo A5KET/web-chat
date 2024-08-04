879032d50932eab271a9ffe759e64ef7eae525ac
A5KET <asketinin@gmail.com>
2024-08-04 09:33:30 +0300

    Added auto growing textarea as chat's input

--- client/src/chat/ChatInputBox.vue
+++ client/src/chat/ChatInputBox.vue
@@ -1,11 +1,14 @@
 <script setup>
-  import ChatTextInput from './ChatTextInput.vue'
+  const emit = defineEmits(['submit'])
+  function onTextAreaInput(event) {
+    const textarea = event.target
+    textarea.style.height = 'auto'
+    textarea.style.height = (textarea.scrollHeight) + "px"
+  }
 </script>
 
 <template>
   <div class="chat-input-box">
-    <div class="text-input">
-      <ChatTextInput />
-    </div>
+    <textarea @input="onTextAreaInput" placeholder="Write a message..." lang="en"></textarea>
   </div>
 </template>
